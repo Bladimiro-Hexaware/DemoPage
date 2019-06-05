346a52eb274b03c7a93092cef4d2570b6767f9ba
Bladimir <bladimiro@hexaware.com>
2019-06-05 11:54:24 -0500

    Date card added

--- main.js
+++ main.js
@@ -1,47 +1,73 @@
-$("#QATitle").attr("title","Hover the title of each cards to get instructions of what to do");
-$("#validateTitle").attr("title","This card validate the two words in the inputs boxes");
-$("#textModalTitle").attr("title","Type any text. The Clear button will empty the text area and the Done button will open a modal with the written text");
-$("#formModal").attr("title","Complete the form to open a new window");
-$("#pageCommentsTitle").attr("title","Type any comment you have about the page (not required)");
-$("#emailTitle").attr("title","Type a valid email address (required)");
-$("#rangeTitle").attr("title","Move the range (not required)");
-$("#dragandDropTitle").attr("title","Drag the Hexaware Logo into the green area");
-$("#rangeTitle").attr("title","Move the range (not required)");
-$("#knowledgeTitlte").attr("title","Select all the technologies that you domain and press the button Ok");
-$("#mapsTitle").attr("title","Click on the button to open a modal with a location inside an iFrame");
-$("#hobbies").attr("title","Select all your hobbies");
+$("#QATitle").attr("title", "Hover the title of each cards to get instructions of what to do");
+$("#validateTitle").attr("title", "This card validate the two words in the inputs boxes");
+$("#textModalTitle").attr("title", "Type any text. The Clear button will empty the text area and the Done button will open a modal with the written text");
+$("#formModal").attr("title", "Complete the form to open a new window");
+$("#pageCommentsTitle").attr("title", "Type any comment you have about the page (not required)");
+$("#emailTitle").attr("title", "Type a valid email address (required)");
+$("#rangeTitle").attr("title", "Move the range (not required)");
+$("#dragandDropTitle").attr("title", "Drag the Hexaware Logo into the green area");
+$("#rangeTitle").attr("title", "Move the range (not required)");
+$("#knowledgeTitlte").attr("title", "Select all the technologies that you domain and press the button Ok");
+$("#mapsTitle").attr("title", "Click on the button to open a modal with a location inside an iFrame");
+$("#hobbies").attr("title", "Select all your hobbies");
 
-function readSelect(){
+
+function calculateDays() {
+    var days = document.getElementById("datepicker").value;
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var output = d.getFullYear() + '-' +
+        (month < 10 ? '0' : '') + month + '-' +
+        (day < 10 ? '0' : '') + day;
+
+
+    if (days != "") {
+        var fecha1 = moment(days);
+        var fecha2 = moment(output);
+        var dif = fecha2.diff(fecha1,'days');
+
+        if(dif < 0)
+            dif = dif * -1;
+
+        $("#selectedDate").text(dif + " days left for your birthday");
+    }
+    else
+     $("#selectedDate").text("Specify a date");
+
+}
+
+function readSelect() {
     var values = $(".domtopics").val();
     $(".selectOutput").val(values)
 }
 
-function Login(){
+function Login() {
     const e = "QA_Team@hexaware.com";
     const p = "Hexaware123";
     var email = document.getElementById("exampleInputEmail1").value;
     var pass = document.getElementById("exampleInputPassword1").value;
 
-    if(email == e && pass == p){
-        window.open("./actions.html","_self");
-    }else{
+    if (email == e && pass == p) {
+        window.open("./actions.html", "_self");
+    } else {
         alert("Invalid Data");
     }
 }
 
 function allowDrop(ev) {
     ev.preventDefault();
-  }
-  
-  function drag(ev) {
+}
+
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
+}
+
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-  }
+}
 
 function Validate() {
     var label = $("#valid").text("");
@@ -84,13 +110,13 @@ document.getElementById("userData").addEventListener("submit", function (event) 
 
     review = document.getElementById("exampleFormControlTextarea1").value;
 
-    if(email != ""
-    && gender != "" 
-    && selectedOption != ""){
+    if (email != ""
+        && gender != ""
+        && selectedOption != "") {
         window.open("./newWindow.html");
-    }else{
+    } else {
         alert("Missing fields in Form");
     }
-    
+
 });
 
